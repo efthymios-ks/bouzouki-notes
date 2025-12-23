9a3f4b7c6318510970cd902ee2e6abf8c9766e62
Efthymios Koktsidis <efthymios.ks@gmail.com>
2025-12-23 18:57:02 +0200

    feat(makam-segments): fill in more details

--- Features/MakamSegments/MakamSegments.js
+++ Features/MakamSegments/MakamSegments.js
@@ -15,6 +15,14 @@ export default [
     ],
   },
   {
+    id: "SAZKAR",
+    name: "Ραστ Σαζκιάρ",
+    intervals: [[3, 1, 2, 2]],
+    leadingInterval: 1,
+    placements: [{ octavePosition: 0, length: 5 }],
+    notes: ["Ό,τι ισχύει και στο Ραστ με μονιμοποίηση της έλξης της 2ης βαθμίδας από την 1η"],
+  },
+  {
     id: "USAK",
     name: "Ουσάκ",
     intervals: [
@@ -28,7 +36,48 @@ export default [
       { octavePosition: 5, length: 4 },
       { octavePosition: 8, length: 4 },
     ],
-    notes: ["Η δεύτερη βαθμίδα έλκεται από τη βάση στις καταληκτικές φράσεις και γίνεται Η-Τ-Τ"],
+    notes: [
+      "Η 2η βαθμίδα έλκεται από την 1η στις καταληκτικές φράσεις (κατιούσα κίνηση) και γίνεται Η-Τ-Τ",
+    ],
+  },
+  {
+    id: "KYRDI",
+    name: "Κιουρντί",
+    intervals: [
+      [1, 2, 2],
+      [1, 2, 2, 2],
+    ],
+    leadingInterval: 2,
+    placements: [
+      { octavePosition: 1, length: 4 },
+      { octavePosition: 1, length: 5 },
+    ],
+  },
+  {
+    id: "BUSELIK",
+    name: "Μπουσελίκ",
+    intervals: [
+      [2, 1, 2],
+      [2, 1, 2, 2],
+    ],
+    leadingInterval: 1,
+    placements: [
+      { octavePosition: 1, length: 4 },
+      { octavePosition: 1, length: 5 },
+    ],
+  },
+  {
+    id: "NIHAVEND",
+    name: "Νιχαβέντ",
+    intervals: [
+      [2, 1, 2],
+      [2, 1, 2, 2],
+    ],
+    leadingInterval: 1,
+    placements: [
+      { octavePosition: 0, length: 4 },
+      { octavePosition: 0, length: 5 },
+    ],
   },
   {
     id: "SEGIAH",
@@ -50,19 +99,6 @@ export default [
     ],
   },
   {
-    id: "KYRDI",
-    name: "Κιουρντί",
-    intervals: [
-      [1, 2, 2],
-      [1, 2, 2, 2],
-    ],
-    leadingInterval: 2,
-    placements: [
-      { octavePosition: 1, length: 4 },
-      { octavePosition: 1, length: 5 },
-    ],
-  },
-  {
     id: "HIJAZ",
     name: "Χιτζάζ",
     intervals: [
@@ -77,7 +113,22 @@ export default [
       { octavePosition: 3, length: 5 },
       { octavePosition: 4, length: 4 },
     ],
-    notes: ["Μπορεί να εμφανιστεί με προσαγωγέα ημιτόνιο όπου και ονομάζεται Ζιργκιουλελί Χιτζάζ"],
+  },
+  {
+    id: "ZIRGULELI_HIJAZ",
+    name: "Ζιργκιουλελί Χιτζάζ",
+    intervals: [[1, 3, 1]],
+    leadingInterval: 1,
+    placements: [{ octavePosition: 1, length: 4 }],
+    notes: ["Ισχύει ό,τι και στο Χιτζάζ, αλλά με προσαγωγέα ημιτόνιο αντί για τόνο"],
+  },
+  {
+    id: "PIRAIOTIKO",
+    name: "Πειραιώτικο",
+    intervals: [[1, 3, 2, 1]],
+    leadingInterval: 1,
+    placements: [{ octavePosition: 0, length: 5 }],
+    notes: ["Παραλλαγή Χιτζάζ με μονιμοποίηση της έλξης της 5ης βαθμίδας από την 4η"],
   },
   {
     id: "NIKRIZ",
